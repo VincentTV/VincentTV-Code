7833d2ccdc86bc3cec6e6128913ea5ff3fa3f6b0
Vincent <vincefong1018@gmail.com>
2019-04-28 04:30:44 +0800

    jQuery火箭

--- Rocket-ScrollToTop/index.js
+++ Rocket-ScrollToTop/index.js
@@ -1,3 +1,7 @@
+/* B站：@VincentTV */
+/* Share with you */
+/* 主页：https://space.bilibili.com/402141442 */
+/* 本视频：https://www.bilibili.com/video/av49252072 */
 $(function() {
   $(window).scroll(function() {
     if($(window).scrollTop() < 100) {
